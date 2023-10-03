09aa40d3d8c7c650ed324b54841557cbb1c88c3e
paulocrazy520 <paulocrazy520@gmail.com>
2023-10-04 02:20:04 +0900

    finish frontend

---
+++ src/layout/navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Box, Button, Container, Typography } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        width: "100vw",
+        height: "80px",
+        zIndex: "100",
+        backdropFilter: "blur(5px)",
+      }}
+    >
+      <Container
+        sx={{
+          height: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "28px",
+            paddingLeft: "10px",
+            paddingRight: "10px",
+            background: "#FF8000",
+            color: "#000000",
+            fontWeight: "900",
+          }}
+        >
+          U.S.DEBT
+        </Typography>
+        <Button
+          sx={{
+            background: "#FF8000",
+            color: "#FFFFFF",
+            fontSize: "14px",
+            textTransform: "none",
+            ":hover": { background: "#FF8000" },
+          }}
+        >
+          Connect Wallet
+        </Button>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar;
